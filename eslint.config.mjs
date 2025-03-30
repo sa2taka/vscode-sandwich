3fc045099753a446cbbfcfa958e939e4e200519b
sa2taka <sa2taka@gmail.com>
2025-03-30 19:54:23 +0900

    🔧 chore: lint and format settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,84 @@
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
+// @ts-check
+import eslint from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import * as eslintPluginImport from "eslint-plugin-import";
+import eslintPluginUnicorn from "eslint-plugin-unicorn";
+import tseslint from "typescript-eslint";
 
-export default [{
-    files: ["**/*.ts"],
-}, {
-    plugins: {
-        "@typescript-eslint": typescriptEslint,
-    },
+const dirname = import.meta.dirname;
 
+export default tseslint.config(
+  eslint.configs.recommended,
+  eslintPluginImport.flatConfigs?.recommended,
+  // @ts-expect-error
+  eslintPluginImport.flatConfigs?.typescript,
+  ...tseslint.configs.strictTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  {
     languageOptions: {
-        parser: tsParser,
-        ecmaVersion: 2022,
-        sourceType: "module",
+      parserOptions: {
+        tsconfigRootDir: dirname,
+        projectService: true,
+      },
     },
-
+  },
+  {
+    files: ["**/*.js"],
+    ...tseslint.configs.disableTypeChecked,
+  },
+  {
     rules: {
-        "@typescript-eslint/naming-convention": ["warn", {
-            selector: "import",
-            format: ["camelCase", "PascalCase"],
-        }],
-
-        curly: "warn",
-        eqeqeq: "warn",
-        "no-throw-literal": "warn",
-        semi: "warn",
+      "@typescript-eslint/prefer-promise-reject-errors": ["off"], // Rule that recommends passing Error to Promise.reject. Turned off because we sometimes pass unknown type.
+      "@typescript-eslint/require-await": ["off"], // Rule that checks if await is used in async functions. Turned off because we have functions that return Promises.
+      "@typescript-eslint/no-explicit-any": ["off"], // Turned off because explicit any is mostly used as a workaround for specific reasons.
+      "@typescript-eslint/no-empty-function": ["off"], // Turned off because empty implementations are usually intentional.
+      "@typescript-eslint/restrict-template-expressions": [
+        "error",
+        {
+          allowNumber: true,
+          allowBoolean: true,
+          allowNullish: true,
+          allowArray: true,
+        },
+      ],
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          args: "all",
+          argsIgnorePattern: "^_",
+          caughtErrors: "all",
+          caughtErrorsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          ignoreRestSiblings: true,
+        },
+      ],
     },
-}];
+  },
+  {
+    settings: {
+      "import/resolver": {
+        typescript: {},
+      },
+    },
+    rules: {},
+  },
+  {
+    plugins: {
+      unicorn: eslintPluginUnicorn,
+    },
+    rules: {
+      "unicorn/better-regex": ["error"],
+      "unicorn/filename-case": [
+        "error",
+        {
+          case: "camelCase",
+        },
+      ],
+    },
+  },
+  {
+    ignores: ["./eslint.config.js", "node_modules/*"],
+  },
+  eslintConfigPrettier
+);
